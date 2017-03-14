c5d323a9b4358633a80ec28d724867ccf34e78a5
HaoZeke <driger633@gmail.com>
2017-03-15 04:38:44 +0530

    Update sources

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -105,6 +105,14 @@ theater.on('type:start, erase:start', function () {
 
 theater.addActor('vader', { speed: 0.8, accuracy: 0.6 }).addActor('luke').addScene('vader:Luke.', 600).addScene('luke:What?', 400).addScene('vader:I am your father.', 400).addScene('luke:Nooo...', -3, '!!! ', 600, 'No! ', 600).addScene('luke:That\'s not true!', 600).addScene('luke:That\'s impossible!', 400).addScene('vader:Search your feelings.', 1600).addScene('vader:You know it to be true.', 1000).addScene('luke:Noooooooo! ', 600, 'No!', 400).addScene('vader:Luke.', 600).addScene('vader:You can destroy the Emperor.', 1600).addScene('vader:He has foreseen this. ', 800).addScene('vader:It is your destiny.', 1600).addScene('vader:Join me.', 800).addScene('vader:Together we can rule the galaxy.', 800).addScene('vader:As father and son.', 1600).addScene('vader:Come with me. ', 800).addScene('vader:It is the only way.', 2000).addScene(theater.replay.bind(theater));
 
+/*
+ * WebFontLoader, added control when using linked fonts via @font-face
+ *
+ * Github repository: 
+ * https://github.com/typekit/webfontloader
+ *
+ */
+
 var WebFont = __webpack_require__(4);
 
 WebFont.load({
